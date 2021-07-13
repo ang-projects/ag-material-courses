3733a8ff15b56e35d9cfe7a80344b54c45dc1acc
Bhavani, Praveen <pkbhavani@gmail.com>
2021-07-13 14:57:01 -0500

    ANG-20202: implemented custom data table, with filtering, sorting, paginating

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -1,8 +1,15 @@
-import {AfterViewInit, Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {CoursesService} from "../services/courses.service";
 import {ActivatedRoute} from "@angular/router";
 import {Course} from "../model/course";
 import {MatTableDataSource} from "@angular/material/table";
+import {DataSource} from "@angular/cdk/collections";
+import {LessonsDataSource} from "../services/lessons.datasource";
+import {MatPaginator} from "@angular/material/paginator";
+import {debounce, debounceTime, distinctUntilChanged, startWith, tap} from "rxjs/operators";
+import {MatSort} from "@angular/material/sort";
+import {fromEvent, merge} from "rxjs";
+
 
 @Component({
   selector: 'app-course',
@@ -12,22 +19,47 @@ import {MatTableDataSource} from "@angular/material/table";
 
 export class CourseComponent implements OnInit, AfterViewInit {
   course: Course;
-  dataSource = new MatTableDataSource([]);
+  dataSource: LessonsDataSource;
   displayedColumns = ["seqNo", "description", "duration"];
+
+  @ViewChild(MatPaginator)
+  paginator: MatPaginator;
+  @ViewChild(MatSort)
+  sort: MatSort;
+  @ViewChild('input')
+  input: ElementRef;
 
   constructor(private route:ActivatedRoute, private courseService: CoursesService) { }
 
   ngOnInit(): void {
     this.course = this.route.snapshot.data["course"];
-    this.courseService.findAllCourseLessons(this.course.id)
-      .subscribe(lessons => this.dataSource.data = lessons);
-  }
-
-  searchLessons(search='') {
-    this.dataSource.filter = search.toLocaleLowerCase().trim();
+    this.dataSource = new LessonsDataSource(this.courseService);
+    this.dataSource.loadLessons(this.course.id, '', 'asc', 0, 4);
   }
 
   ngAfterViewInit(): void {
+    fromEvent(this.input.nativeElement, 'keyup')
+      .pipe(
+        debounceTime(150),
+        distinctUntilChanged(),
+        tap(() => {
+          this.paginator.pageIndex = 0;
+          this.loadLessonsPage();
+        })
+      ).subscribe();
 
+    this.sort.sortChange.subscribe(() => this.paginator.pageIndex = 0);
+    merge(this.sort.sortChange, this.paginator.page)
+      .pipe(
+       tap(() => this.loadLessonsPage())
+      )
+      .subscribe()
+  }
+  loadLessonsPage() {
+    this.dataSource.loadLessons(this.course.id,
+      this.input.nativeElement.value,
+      this.sort.direction,
+      this.paginator.pageIndex,
+      this.paginator.pageSize)
   }
 }
